57b3c30fd5a847d50c179fec0fd8e8e082193da7
keks2003 <olecsa30032003@gmail.com>
2020-11-03 15:53:17 +0000

    Added sample markup

--- js/index.js
+++ js/index.js
@@ -1,37 +1,28 @@
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date())
-  return {
-    'total': t,
-    'days': Math.floor(t / (1000 * 60 * 60 * 24)),
-    'hours': Math.floor((t / (1000 * 60 * 60)) % 24),
-    'minutes': Math.floor((t / 1000 / 60) % 60),
-    'seconds': Math.floor((t / 1000) % 60)
-  };
-}
- 
-function initializeClock(selector, endtime) {
-  const clock = document.querySelector(selector);
-  const days = clock.querySelector('.day.number');
-  const hours = clock.querySelector('.hour.number');
-  const minutes = clock.querySelector('.minute.number');
-  const seconds = clock.querySelector('.second.number');
- 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
- 
-    days.innerHTML = t.days;
-    hours.innerHTML = ('0' + t.hours).slice(-2);
-    minutes.innerHTML = ('0' + t.minutes).slice(-2);
-    seconds.innerHTML = ('0' + t.seconds).slice(-2);
- 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
- 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
- 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-initializeClock('.timer-container', deadline);
+const burgerMenu = () => {
+
+        const menu = document.querySelector('.burger'),
+            menuNav = document.querySelector('.nav-list'),
+            body = document.querySelector('body');
+
+        body.addEventListener('click', (event) => {
+
+            let target = event.target;
+
+            if (target.closest('.burger')) {
+                menu.classList.add('burger-open');
+                menuNav.style.display = 'block';
+            } else if (target.classList.contains('burger')) {
+                menu.classList.remove('burger-open');
+                menuNav.style.display = 'none';
+            } else if (target.tagName !== 'DIV') {
+                menu.classList.remove('burger-open');
+                menuNav.style.display = 'none';
+            } else {
+                return;
+            }
+            
+        });
+
+    };
+
+    burgerMenu();
